5de97543c7713ad5f2c74bd63d9cc8a95655f157
Yucheng Zheng <yuchengmojian@gmail.com>
2020-03-16 21:42:19 -0700

    Edit method half complete

--- src/address/gui/AddressBookGui.java
+++ src/address/gui/AddressBookGui.java
@@ -24,11 +24,14 @@ public class AddressBookGui {
     private JList<AddressEntry> allList;
     private JList<AddressEntry> findList;
     private DefaultListModel<AddressEntry> model;
+    private DefaultListModel<AddressEntry> findModel;
     private JScrollPane allPane;
     private JScrollPane findPane;
+    private JButton editButton;
     private AddressBook ab;
 
     public AddressBookGui() {
+
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -36,7 +39,7 @@ public class AddressBookGui {
                 dialog.pack();
                 dialog.setVisible(true);
                 //create a temporary addressEntry object to hold the return value
-                AddressEntry newEntry = new AddressEntry();
+                AddressEntry newEntry;
                 newEntry = dialog.getEntry();
 
                 //push the new entry to data base
@@ -113,9 +116,59 @@ public class AddressBookGui {
         findButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                FindDialog dialog = new FindDialog();
+                FindDialog dialog = new FindDialog(ab);
                 dialog.pack();
                 dialog.setVisible(true);
+
+                //clear for a fresh model
+                List<AddressEntry> selected = dialog.getSelected();
+                findModel.clear();
+                for(int i = 0; i < selected.size(); i++){
+                    findModel.addElement(selected.get(i));
+                }
+                findList.setModel(findModel);
+            }
+        });
+
+        editButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AddressEntry toEdit = new AddressEntry();
+                toEdit = findList.getSelectedValue();
+                EditDialog dialog = new EditDialog(toEdit);
+                dialog.pack();
+                dialog.setVisible(true);
+
+                //gets the returned change from edit dialog
+                toEdit = dialog.getEdited();
+
+                //Update changes to Database
+                try {
+                    editDB(toEdit);
+                } catch (ClassNotFoundException ex) {
+                    ex.printStackTrace();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+
+                //fresh read of contents from from database
+                try {
+                    ab.readFromDB();
+                } catch (ClassNotFoundException ex) {
+                    ex.printStackTrace();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+
+                //clear for a fresh model
+                ArrayList<AddressEntry> newList = ab.getList();
+                model.clear();
+                for(int i = 0; i < newList.size(); i++){
+                    model.addElement(newList.get(i));
+                }
+                allList.setModel(model);
+
+                //clear for a fresh model
             }
         });
     }
@@ -134,11 +187,29 @@ public class AddressBookGui {
         for(int i = 0; i < newList.size(); i++){
             model.addElement(newList.get(i));
         }
+
         allList.setModel(model);
         allList.setLayoutOrientation(JList.VERTICAL);
         allList.setCellRenderer(new AddressEntryRenderer());
-
         allList.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 0));
+
+        // Set to something...
+        /*
+        ArrayList<AddressEntry> selected = new ArrayList<>();
+
+        if (findList != null || findList.size() < 1) {
+            for(int i = 0; i < newList.size(); i++){
+                findModel.addElement(selected.get(i));
+            }
+        }
+        *
+         */
+
+        findModel = new DefaultListModel<AddressEntry>();
+        findList.setModel(findModel);
+        findList.setLayoutOrientation(JList.VERTICAL);
+        findList.setCellRenderer(new AddressEntryRenderer());
+        findList.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 0));
 
         myFrame = new JFrame("AddressBookGui");
         myFrame.setContentPane(mainPanel);
@@ -178,5 +249,22 @@ public class AddressBookGui {
         conn.close();
     }
 
+    public void editDB(AddressEntry e) throws ClassNotFoundException, SQLException {
+        Class.forName("oracle.jdbc.OracleDriver");
+        Connection conn =
+                DriverManager.getConnection("jdbc:oracle:thin:mcs1003/85kTyIfb@adcsdb01.csueastbay.edu:1521/mcspdb.ad.csueastbay.edu");
+        Statement stmt = conn.createStatement();
+        //ID auto increment from data base sequence
+        stmt.executeQuery("UPDATE ADDRESSENTRYTABLE SET FIRSTNAME = '" + e.getName().getFirstName() +
+                        "', LASTNAME = '" + e.getName().getLastName() +
+                        "', STREET = '" + e.getAddress().getStreet() +
+                        "', CITY = '" + e.getAddress().getCity() +
+                        "', STATE = '" + e.getAddress().getState() +
+                        "', ZIP = " + e.getAddress().getZip() +
+                        ", EMAIL = '" + e.getEmail() +
+                        "', PHONE = '" + e.getPhone() + "' WHERE ID = " + e.getID());
+        stmt.close();
+        conn.close();
+    }
 
 }
--- src/address/gui/FindDialog.java
+++ src/address/gui/FindDialog.java
@@ -1,22 +1,28 @@
 package address.gui;
 
+import address.data.AddressBook;
+import address.data.AddressEntry;
+
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.List;
 
 public class FindDialog extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
     private JTextField findField;
+    private JLabel headerLabel;
+    private List<AddressEntry> selected;
 
-    public FindDialog() {
+    public FindDialog(AddressBook ab) {
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
 
         buttonOK.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                onOK();
+                onOK(ab);
             }
         });
 
@@ -42,9 +48,17 @@ public class FindDialog extends JDialog {
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
-    private void onOK() {
-        // add your code here
-        dispose();
+    private void onOK(AddressBook ab) {
+        if (findField.getText().length() > 0 && ab.find(findField.getText()).size() > 0) {
+            dispose();
+            selected = ab.find(findField.getText());
+        } else {
+            headerLabel.setText("No matching entries were found");
+        }
+    }
+
+    public List<AddressEntry> getSelected() {
+        return selected;
     }
 
     private void onCancel() {
